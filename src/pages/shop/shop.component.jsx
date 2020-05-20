19763fb157282dce64a96a7f728d9d18f0fa712b
kaly <kalimamadou@gmail.com>
2020-05-19 20:08:12 -0700

    Added Collection Page to display each collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import { Route } from "react-router-dom";
 
-const ShopPage = ({ collections }) => {
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionsPage from "../collections/collections.component";
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionsPage} />
     </div>
   );
 };
